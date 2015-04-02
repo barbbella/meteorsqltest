f636be24d0591f98531496a5e5130249d8eb8bda
Kate Jefferson <barbedbella@gmail.com>
2015-04-02 12:30:24 -0500

    (fix) setup

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var pg = require('pg');
 var async = require('async');
-var conString = 'postgres://postgres:1324@localhost/dvdrental';
+var conString = 'postgres://postgres:1234@localhost/dvdrental';
 var app = express();
 
 // view engine setup
@@ -39,38 +39,97 @@ var film = function(id, title) {
   this.title = title;
 };
 
-
-var insert = function() {
-  console.log('got into insertMessagesFromTrees is ');
-  // console.log(treeid, userid, message);
+var createIndex = function (){
   pg.connect(conString, function(err, client, done) {
-    // console.log(err);
-    var selectMessages = 'INSERT INTO message (message, treeid, userid, createdat) values($1, $2, $3, now())';
-    client.query(selectMessages, [message, treeid, userid], function(error, results) {
-      // console.log('results is ', results);
-      res.send(results);
+    console.log(err);
+    var film = "create index on film_docs using GIN(data)";
+    client.query(film, function(error, results) {
+      console.log(results);
       done();
+      callback(null);
     });
   });
 };
 
 
-var find = function(item1, item2, item3) {
-  var userArray = [];
+var insert = function(callback) {
+  pg.connect(conString, function(err, client, done) {
+    console.log(err);
+    var film = {"title": "Test Italian", "length": 117, "rating": "NC-17", "film_id": 133, "fulltext": "'chamber':1 'fate':4 'husband':11 'italian':2 'monkey':16 'moos':8 'must':13 'nigeria':18 'overcom':14 'reflect':5", "description": "A Fateful Reflection of a Moose And a Husband who must Overcome a Monkey in Nigeria", "language_id": 1, "last_update": "2013-05-26T14:50:58.951", "rental_rate": 4.99, "release_year": 2006, "rental_duration": 7, "replacement_cost": 14.99, "special_features": ["Trailers"]};
+    var insert = 'INSERT INTO film_docs (data) values ($1)';
+    client.query(insert, [film], function(error, results) {
+      console.log("error in inserts", error);
+      console.log("results in inserts", results);
+      done();
+      callback(null);
+    });
+  });
+};
+
+var find = function(element, callback) {
   pg.connect(conString, function(err, client, done){
-  console.log(err);
-    var find = 'SELECT $1, $2 from $3';
-    client.query(find, [item1, item2, item3], function(error, results) {
-      for (var x = 0, count = results.rows; x < count; x++) {
-        userArray.push(new film(results.rows[x].film_id, results.rows[x].title));
-      }
-      return userArray;
+    console.log(err);
+    //console.log(client, 1234);
+    var element = {title: "Test Italian"};
+    var find = "SELECT (data ->> 'title') as Title, (data -> 'length') as Length FROM film_docs WHERE data @> $1";
+    client.query(find, [element], function(error, results) {
+      console.log("find results", results);
+      console.log("find error", error);
+      //console.log(results);
       done();
+      callback(null);
     });
   });
 };
 
-console.log(find());
+var update = function(callback) {
+  pg.connect(conString, function(err, client, done){
+    console.log(err);
+    var data = {title: "Chamber Italian"};
+    var newdata = {"title": "Test test", "length": 117, "rating": "NC-17", "film_id": 133, "fulltext": "'chamber':1 'fate':4 'husband':11 'italian':2 'monkey':16 'moos':8 'must':13 'nigeria':18 'overcom':14 'reflect':5", "description": "A Fateful Reflection of a Moose And a Husband who must Overcome a Monkey in Nigeria", "language_id": 1, "last_update": "2013-05-26T14:50:58.951", "rental_rate": 4.99, "release_year": 2006, "rental_duration": 7, "replacement_cost": 14.99, "special_features": ["Trailers"]};
+    var update = "update film_docs set data = $1 where data @> $2";
+    client.query(update, [data], function(error, results){
+      console.log("error in update", error);
+      console.log("error in results", results);
+      done();
+      callback(null);
+    })
+  })
+};
+
+
+
+//var userArray = [];
+//var find = function(callback) {
+//  pg.connect(conString, function(err, client, done){
+//  console.log(err);
+//    //console.log(client, 1234);
+//    var find = 'SELECT film_id, title from film limit 100';
+//    client.query(find, function(error, results) {
+//      console.log(results);
+//      //console.log(results);
+//      for (var x = 0, count = results.rows.length; x < count; x++) {
+//        userArray.push(new film(results.rows[x].film_id, results.rows[x].title));
+//      }
+//      done();
+//      callback(null);
+//    });
+//  });
+//};
+
+async.series([
+  //function(callback){
+  //  insert(callback);
+  //},
+  function(callback){
+    find("hello", callback);
+  },
+  function(callback){
+    update(callback);
+  }
+]);
+//find('film_id', 'title');
+//console.log(userArray);
 
 // error handlers
 
